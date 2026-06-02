59859d1346ba705c6193cd76a1939675fc4d0a12
Israel Hamdan <IsraelSantos_20k@outlook.com>
2026-06-02 12:44:17 -0300

    feat: configurando conexão com o db e definindo modelagem de dados

---
+++ Entities/Todo.cs
@@ -0,0 +1,29 @@
+﻿namespace TaskTelemetry.Entities;
+
+public class Todo
+{
+  public Guid Id { get; private set; }
+  public string Title { get; private set; }
+  public string Description { get; private set; }
+  public DateTime CreatedAt { get; private set; }
+  public DateTime UpdatedAt { get; private set; }
+  public DateTime Deadline { get; private set; }
+  public bool IsCompleted { get; private set; }
+  public Guid UserId { get; set; }
+  public User User { get; private set; }
+  
+  public Todo()
+  {
+  }
+
+  public Todo(Guid id, string title, string description, DateTime createdAt, DateTime updatedAt, DateTime deadline, bool isCompleted)
+  {
+    Id = id;
+    Title = title;
+    Description = description;
+    CreatedAt = createdAt;
+    UpdatedAt = updatedAt;
+    Deadline = deadline;
+    IsCompleted = isCompleted;
+  }
+}
